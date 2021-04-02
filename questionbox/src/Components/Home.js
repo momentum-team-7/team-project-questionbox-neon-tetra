bc9ac8d30b01289c028ad97b3a7765164bc297f2
Tip Powell <powelltip@gmail.com>
2021-04-02 16:49:19 -0400

    dot map working now

--- questionbox/src/Components/Home.js
+++ questionbox/src/Components/Home.js
@@ -10,15 +10,8 @@ export default function Home() {
     return (
         <div>
             <h1>Question Feed</h1>
-                {questions.map((question) => (
                     <QuestionFeed
-                    key = {question.id}
-                    question_title = {question.title}
-                    date_created = {question.date_created}
-                    question_body = {question.body}
-                    question_id = {question.id}
                     />
-                ))}
         </div>
     )
 }
--- questionbox/src/Components/QuestionFeed.js
+++ questionbox/src/Components/QuestionFeed.js
@@ -1,20 +1,25 @@
 // import axios from 'axios'
 // import { useEffect } from 'react'
 import QuestionDetail from './QuestionDetail'
+import data from '../questions2'
+import React, { useState } from 'react'
 
 
-const QuestionFeed = ({ question_title, date_created, question_body, question_id }) => {
-
+const QuestionFeed = ({  }) => {
+const [questions, setQuestions] = useState(data)
+console.log(questions)
 
 
     return (
         <div>
+            {questions.map((question) => (
             <QuestionDetail
-                question_title = {question_title}
-                date_created = {date_created}
-                question_body = {question_body}
-                question_id = {question_id}
+                question_title = {question.title}
+                date_created = {question.date_created}
+                question_body = {question.body}
+                key = {question.id}
             />
+            ))}
         </div>
     )
 }
