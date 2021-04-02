2ced44f4f19cf374b6b958dec1bb252d907a4e1d
cdiazit5 <cdiazit5@gmail.com>
2021-04-02 15:58:19 -0400

    trying to move the question mapping to the question feed

--- questionbox/src/Components/QuestionFeed.js
+++ questionbox/src/Components/QuestionFeed.js
@@ -1,20 +1,26 @@
 // import axios from 'axios'
 // import { useEffect } from 'react'
-import QuestionDetail from './QuestionDetail'
+import Question from './Question'
+import React, { useState } from 'react'
+import data from '../questions2'
 
 
 const QuestionFeed = ({ question_title, date_created, question_body, question_id }) => {
+    const [questions, setQuestion] = useState(data)
 
 
 
     return (
         <div>
-            <QuestionDetail
-                question_title = {question_title}
-                date_created = {date_created}
-                question_body = {question_body}
-                question_id = {question_id}
-            />
+            {questions.map((question, index) => (
+                <Question
+                key = {index}
+                question_title = {question.question_title}
+                date_created = {question.date_created}
+                question_body = {question.question_body}
+                question_id = {question.question_id}
+                />
+            ))}
         </div>
     )
 }
